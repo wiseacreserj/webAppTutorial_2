a35346fe34ea9a5e784f8a6074e5f707859e22b3
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2021-01-16 18:27:43 +0200

    Project ended

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const initApp = () => {
     const list = document.querySelector(".nav__list");
     menuBtn.addEventListener("click", toggleButtonAndMenu);
     list.addEventListener("click", toggleButtonAndMenu);
+    displayCurentYear();
 };
 
 const toggleButtonAndMenu = () => {
@@ -14,4 +15,9 @@ const toggleButtonAndMenu = () => {
     const list = document.querySelector(".nav__list");
     menuBtn.classList.toggle("open");
     list.classList.toggle("open");
+};
+
+const displayCurentYear = () => {
+    const year = new Date().getFullYear();
+    document.getElementById("year").textContent = year;
 };
